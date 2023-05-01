6a852521615e89601cb22e48d4f9eb02e7a98ff9
Santiago Tovar <santiagocentenot@gmail.com>
2023-05-01 16:16:00 -0400

    Primer commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Inicio de aplicacion de calendario
+
+1. se istala react con vite , incluido react router ,boostrap y font awesome
+
+2. se crea el directorio auth y el directorio calendar, se crea una pagina en cada directorio
+
+3. se crea un directorio llamado router con el AppRouter.jsx donde se crean las rutas correspondientes
+
+4. se manda a llamar el app router en el Calendar.jsx y envolvemos en BroserRouter para finalmente llamarlo al main
---
+++ src/store/store.js
@@ -0,0 +1,16 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { uiSlice } from "./ui/uiSlice";
+import { calendarSlice } from "./calendar/calendarSlice";
+import { authSlice } from "./auth/authSlice";
+
+export const store = configureStore({
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+  reducer: {
+    ui: uiSlice.reducer,
+    calendar: calendarSlice.reducer,
+    auth: authSlice.reducer,
+  },
+});
